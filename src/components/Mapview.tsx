7e4ca775623f5e5f100c207d7f8e3b4328e09a44
seo-sang-ho <rocter2049@naver.com>
2025-11-04 01:09:31 +0900

    지도 마커 + 인포윈도우 기능 추가

--- src/components/Mapview.tsx
+++ src/components/Mapview.tsx
@@ -13,7 +13,53 @@ function App() {
       level: 3, //
     };
 
-    new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+    const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      const locPosition = new kakao.maps.LatLng(lat,lon),
+      message = '<div style="padding:5px; color:black">현재 위치</div>';
+
+      displayMarker(locPosition,message);
+
+      });
+
+      // geolocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용 설정
+    } else{
+
+      const locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+      message = 'geolacaiton을 사용할 수 없습니다.';
+
+      displayMarker(locPosition,message);
+    }
+
+    // 지도에 마커와 인포윈도우 표시하는 함수
+    function displayMarker(locPosition,message){
+
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: locPosition
+      });
+
+      // 인포윈도우에 표시할 내용
+      const iwContent = message, 
+            iwRemoveable = true;
+
+      // 인포윈도우 생성
+      const infowindow = new kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemoveable
+      });
+
+      infowindow.open(map,marker);
+
+      map.setCenter(locPosition);
+    };
+
   }, []);
 
   return (
